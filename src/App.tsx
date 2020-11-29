a236b8a7f2e664fc27be1dc1d9f8f3b864253660
YeseungPark <parkyes90@gmail.com>
2020-11-29 22:46:11 +0900

    Add Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,87 @@
-import React from 'react';
+import React from "react";
+import { Chip, Grid, makeStyles, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  };
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-   <div>
-     App
-   </div>
+    <div className={classes.root}>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>
+            <Grid container justify="flex-start">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>
+            <Grid container justify="flex-start">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>
+            <Grid container justify="flex-start">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>
+            <Grid container justify="flex-start">
+              <Grid item>
+                <Chip label="xs=12" />
+              </Grid>
+              <Grid item>
+                <Chip label="sm=6" />
+              </Grid>
+              <Grid item>
+                <Chip label="md=3" />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
